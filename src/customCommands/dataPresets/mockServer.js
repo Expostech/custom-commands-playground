fc91398dd96eca7e00d13c1b2dfe02d9b68d329b
niekcandaele <niek@candaele.dev>
2021-03-07 13:39:14 +0100

    Add server stats object

--- src/customCommands/dataPresets/mockServer.js
+++ src/customCommands/dataPresets/mockServer.js
@@ -1,5 +1,6 @@
-import faker from 'faker'
-import mockPlayer from './mockPlayer'
+import faker from 'faker';
+
+import mockPlayer from './mockPlayer';
 
 function mockServer() {
     return {
@@ -9,7 +10,18 @@ function mockServer() {
         name: faker.company.companyName(),
         ip: faker.internet.ip(),
         webPort: faker.random.number(),
-        onlinePlayers: [...Array(faker.random.number({ max: 10, min: 1 }))].map(_ => mockPlayer())
+        onlinePlayers: [...Array(faker.random.number({ max: 10, min: 1 }))].map(_ => mockPlayer()),
+        stats: {
+            gametime: {
+                days: faker.random.number({ min: 1, max: 250 }),
+                hours: faker.random.number({ min: 0, max: 24 }),
+                minutes: faker.random.number({ min: 1, max: 60 })
+            },
+            players: faker.random.number({ min: 1, max: 20 }),
+            hostiles: faker.random.number({ min: 1, max: 100 }),
+            animals: faker.random.number({ min: 1, max: 100 })
+        }
+
     }
 }
 
