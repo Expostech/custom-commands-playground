b68236ad1fc9542b9fddac78aaea6ac436c77660
Niek Candaele <niek@candaele.dev>
2021-08-19 21:07:11 +0200

    Set path of playground

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Playground, PageNotFound } from './pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { PageNotFound, Playground } from './pages';
 
 export const Router: FC = () => (
-  <BrowserRouter>
+  <BrowserRouter >
     <Routes>
-      <Route element={<Playground />} path="/" />
+      <Route element={<Playground />} path="/playground" />
       <Route element={<PageNotFound />} path="*" />
     </Routes>
   </BrowserRouter>
