797b8066642a778153a109576e70c08df0d709b6
Niek Candaele <niek@candaele.dev>
2022-01-02 22:50:38 +0100

    feat: add Sentry error reporting

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -91,6 +91,8 @@ export const Playground: FC = () => {
 
   return (
     <PlaygroundContainer>
+      {/*@ts-expect-error aa*/}
+      <button onClick={methodDoesNotExist}>Break the world</button>
       <ExecuteButton onClick={executeCommand} />
       <EditorContainer ref={ref} />
       <RightContainer>
