b2b31bd6cdf65cdb83106445e825595214039564
Expostech <expostech@ubuntu>
2022-11-25 22:03:50 +1030

    feat: simple version system

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,35 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const SettingsTitle = styled.div`
+  color: black;
+`;
+
+const VersionContainer = styled.div`
+  color: black;
+`;
+
+export interface IVersionData {
+  version: string;
+}
 
 export const Settings: FC = () => {
-  return <div>settings</div>;
+  const [version, setVersion] = useState<IVersionData | null>(null);
+
+  useEffect(() => {
+    const getVersion = async () => {
+      const data = await fetch('version.json');
+      const jsonData = await data.json();
+      setVersion(jsonData);
+    };
+
+    getVersion();
+  }, []);
+
+  return (
+    <>
+      <SettingsTitle>Settings</SettingsTitle>
+      <VersionContainer>Version: {version?.version}</VersionContainer>
+    </>
+  );
 };
